20ec837f6d83fc9b829be0d5de25e52817039619
Mynot <stefan.schweifer@gmail.com>
2021-11-28 11:39:03 +0100

    Events for all Buttons

--- js/app.js
+++ js/app.js
@@ -1,32 +1,48 @@
-const button1 = document.getElementById('Button_1');
-const button2 = document.getElementById('Button_2');
-const button3 = document.getElementById('Button_3');
-const button4 = document.getElementById('Button_4');
+const section1 = document.getElementById('Button_1');
+const section2 = document.getElementById('Button_2');
+const section3 = document.getElementById('Button_3');
+const section4 = document.getElementById('Button_4');
 
-
-
-button1.addEventListener('mouseover', setButtonBackground)
-    // function () {
-    //  document.getElementById("Button_1").style.backgroundColor = 'grey';
-    //}
-    ;
-button1.addEventListener('mouseout', setbackButtonBackground)
-    // function () {
-    //  document.getElementById("Button_1").style.backgroundColor = 'grey';
-    //}
-    ;
-
-
-function setButtonBackground() {
-    button1.style.backgroundColor = 'grey';
+//Change Appearence Finctions for Events
+//Section 1
+function setButtonBackgroundS1() {
+    section1.style.backgroundColor = 'grey';
 }
-function setbackButtonBackground() {
-    button1.style.backgroundColor = 'aliceblue';
+function setbackButtonBackgroundS1() {
+    section1.style.backgroundColor = 'aliceblue';
 }
-//button1.addEventListener('mouseenter', setButtonBackground(button1), false)
-    // function () {
-    //   document.getElementById("Button_1").style.backgroundColor = 'grey';
-    //}
-  //  ;
+//Section 2
+function setButtonBackgroundS2() {
+    section2.style.backgroundColor = 'grey';
+}
+function setbackButtonBackgroundS2() {
+    section2.style.backgroundColor = 'aliceblue';
+}
+//Section 3
+function setButtonBackgroundS3() {
+    section3.style.backgroundColor = 'grey';
+}
+function setbackButtonBackgroundS3() {
+    section3.style.backgroundColor = 'aliceblue';
+}
+//Section 4
+function setButtonBackgroundS4() {
+    section4.style.backgroundColor = 'grey';
+}
+function setbackButtonBackgroundS4() {
+    section4.style.backgroundColor = 'aliceblue';
+}
 
-//button1.onpointerenter = setButtonBackground(button2);
+//Button Events
+//Section 1
+section1.addEventListener('mouseover', setButtonBackgroundS1);
+section1.addEventListener('mouseout', setbackButtonBackgroundS1);
+//Section 2
+section2.addEventListener('mouseover', setButtonBackgroundS2);
+section2.addEventListener('mouseout', setbackButtonBackgroundS2);
+//Section 3
+section3.addEventListener('mouseover', setButtonBackgroundS3);
+section3.addEventListener('mouseout', setbackButtonBackgroundS3);
+//Section 4
+section4.addEventListener('mouseover', setButtonBackgroundS4);
+section4.addEventListener('mouseout', setbackButtonBackgroundS4);
