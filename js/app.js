cd14e790f350d0dc365acf62c3427947acfdd55e
Mynot <stefan.schweifer@gmail.com>
2021-12-02 20:22:37 +0100

    New HTML structure

--- js/app.js
+++ js/app.js
@@ -3,34 +3,42 @@ const section2 = document.getElementById('Button_2');
 const section3 = document.getElementById('Button_3');
 const section4 = document.getElementById('Button_4');
 
-//Change Appearence Finctions for Events
+//Change Appearence Functions for Events
 //Section 1
 function setButtonBackgroundS1() {
-    section1.style.backgroundColor = 'grey';
+    section1.style.backgroundImage = 'linear-gradient(to right, DodgerBlue, white)';
+    section1.style.boxShadow = '1px 1px 1px 1px #888888';
 }
 function setbackButtonBackgroundS1() {
-    section1.style.backgroundColor = 'aliceblue';
+    section1.style.backgroundImage = '';
+    section1.style.boxShadow = '';
 }
 //Section 2
 function setButtonBackgroundS2() {
-    section2.style.backgroundColor = 'grey';
+    section2.style.backgroundImage = 'linear-gradient(to right, DodgerBlue, white)';
+    section2.style.boxShadow = '1px 1px 1px 1px #888888';
 }
 function setbackButtonBackgroundS2() {
-    section2.style.backgroundColor = 'aliceblue';
+    section2.style.backgroundImage = '';
+    section2.style.boxShadow = '';
 }
 //Section 3
 function setButtonBackgroundS3() {
-    section3.style.backgroundColor = 'grey';
+    section3.style.backgroundImage = 'linear-gradient(to right, DodgerBlue, white)';
+    section3.style.boxShadow = '1px 1px 1px 1px #888888';
 }
 function setbackButtonBackgroundS3() {
-    section3.style.backgroundColor = 'aliceblue';
+    section3.style.backgroundImage = '';
+    section3.style.boxShadow = '';
 }
 //Section 4
 function setButtonBackgroundS4() {
-    section4.style.backgroundColor = 'grey';
+    section4.style.backgroundImage = 'linear-gradient(to right, DodgerBlue, white)';
+    section4.style.boxShadow = '1px 1px 1px 1px #888888';
 }
 function setbackButtonBackgroundS4() {
-    section4.style.backgroundColor = 'aliceblue';
+    section4.style.backgroundImage = '';
+    section4.style.boxShadow = '';
 }
 
 //Button Events
