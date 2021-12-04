8716beda4c8aed832e2501b60b55ccba91147859
Mynot <stefan.schweifer@gmail.com>
2021-12-04 20:27:23 +0100

    dynam Content, active view

--- js/app.js
+++ js/app.js
@@ -1,7 +1,98 @@
+
+
+
+//Get Data
+
+
+sections = document.querySelectorAll('section');
+
+sectionwcontent = document.getElementsByClassName('content-container');
+
+
+const contentlength = document.getElementsByClassName('content-container').length;
+
+for (let i = 0; i < contentlength; i++) {
+
+    sectionname = sectionwcontent[i].getAttribute('data-navi');
+    createNavigationButton(i + 1, sectionname)
+
+    createMainContentSection(i + 1, sectionname)
+
+};
+
+
+
+
 const section1 = document.getElementById('Button_1');
 const section2 = document.getElementById('Button_2');
 const section3 = document.getElementById('Button_3');
 const section4 = document.getElementById('Button_4');
+
+const mainsection1 = document.getElementById('section_1');
+const mainsection2 = document.getElementById('section_2');
+const mainsection3 = document.getElementById('section_3');
+const mainsection4 = document.getElementById('section_4');
+
+
+
+
+//Set Data
+//Create Navigation Element
+function createNavigationButton(elementno, secname) {
+    //create Elements
+    let newNav = document.createElement('div');
+    let newContent = document.createTextNode(secname);
+    //Set Attributes
+    newNav.setAttribute('class', 'section');
+    newNav.setAttribute('id', 'Button_' + elementno);
+    newNav.appendChild(newContent);
+    //Insert new element in Naviagation
+
+    const navigation = document.querySelector('nav');
+
+    navigation.appendChild(newNav);
+
+}
+
+//Create Main Content Sections
+function createMainContentSection(elementno, secname) {
+    //create Elements
+    let newHead = document.createElement('h1');
+    let newHdContent = document.createTextNode(secname);
+
+    //Set Attributes
+    newHead.appendChild(newHdContent);
+
+    //Insert new element in Naviagation
+    const section = 'section_' + elementno;
+    const mainsection = document.getElementById(section);
+
+    mainsection.appendChild(newHead);
+    mainsection.insertAdjacentHTML("beforeend", maintext);
+
+}
+
+// Function set active state
+
+function setActiveState() {
+    for (const section of sections) {
+        const box = section.getBoundingClientRect();
+        const sectionOffset = section.offsetTop;
+        const sectionHight = screen.height;
+        const boxtop = box.top;
+        const boxbutton = box.bottom;
+
+        if (boxtop <= (sectionHight / 2)
+            && boxbutton >= (sectionHight / 2)
+        ) {
+            section.setAttribute('class', 'Active');
+        } else {
+            section.setAttribute('class', '');
+        }
+    }
+}
+
+
 
 //Change Appearence Functions for Events
 //Section 1
@@ -12,6 +103,18 @@ function setButtonBackgroundS1() {
 function setbackButtonBackgroundS1() {
     section1.style.backgroundImage = '';
     section1.style.boxShadow = '';
+}
+function selectViewS1() {
+    mainsection1.scrollIntoView(true);
+}
+function selectViewS2() {
+    mainsection2.scrollIntoView(true);
+}
+function selectViewS3() {
+    mainsection3.scrollIntoView(true);
+}
+function selectViewS4() {
+    mainsection4.scrollIntoView(true);
 }
 //Section 2
 function setButtonBackgroundS2() {
@@ -41,16 +144,25 @@ function setbackButtonBackgroundS4() {
     section4.style.boxShadow = '';
 }
 
+document.addEventListener("scroll", setActiveState);
+
 //Button Events
 //Section 1
 section1.addEventListener('mouseover', setButtonBackgroundS1);
 section1.addEventListener('mouseout', setbackButtonBackgroundS1);
+section1.addEventListener('click', selectViewS1)
 //Section 2
 section2.addEventListener('mouseover', setButtonBackgroundS2);
 section2.addEventListener('mouseout', setbackButtonBackgroundS2);
+section2.addEventListener('click', selectViewS2)
 //Section 3
 section3.addEventListener('mouseover', setButtonBackgroundS3);
 section3.addEventListener('mouseout', setbackButtonBackgroundS3);
+section3.addEventListener('click', selectViewS3)
 //Section 4
 section4.addEventListener('mouseover', setButtonBackgroundS4);
 section4.addEventListener('mouseout', setbackButtonBackgroundS4);
+section4.addEventListener('click', selectViewS4)
+
+
+
---
+++ js/content.js
@@ -0,0 +1,42 @@
+
+
+let maintext = String.raw`<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et molestie est, quis imperdiet
+odio. Aenean lorem nunc, consequat a nisl vitae, congue facilisis lacus. In eu leo id lacus
+dignissim consectetur eu eu ipsum. Sed consectetur consequat arcu nec porttitor. Proin sem orci,
+vehicula iaculis porta vitae, pellentesque aliquam nisi. Donec vehicula massa sapien, at
+vehicula tortor tempus sit amet. Proin id dolor facilisis, feugiat enim id, feugiat nibh.
+Vivamus enim sapien, molestie quis rhoncus in, volutpat in urna. Pellentesque ac risus et tortor
+vestibulum pretium in in mauris.
+</p>
+<p>Donec luctus, urna eu ornare viverra, lectus orci porta enim, eget dapibus sapien eros eu lacus.
+Sed bibendum risus ut pellentesque pharetra. Etiam mauris dolor, porta in venenatis sed,
+porttitor vel nunc. Proin suscipit lacus et ligula fringilla, id rhoncus neque rhoncus. Praesent
+quis turpis urna. Curabitur vel turpis in nibh condimentum tincidunt non vitae nisl. Nullam nec
+pretium nisi. Vivamus molestie id lorem at condimentum. Donec dignissim sagittis congue. Donec
+sagittis tempor tempor. Sed lacinia nunc ut dui convallis egestas. Suspendisse faucibus blandit
+erat ac blandit. Nulla ligula purus, cursus vel urna sed, maximus pharetra mauris. Fusce eu
+lacus lobortis, pellentesque justo ac, mollis mi. Aliquam eleifend fermentum massa ac vehicula.
+</p>
+<p>Morbi rutrum arcu id nunc luctus, non pulvinar diam accumsan. Cras at enim a urna malesuada
+pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+curae; Donec nec dolor sit amet ante maximus eleifend. Donec mattis commodo leo. Ut rhoncus
+consectetur ante nec convallis. Aenean rutrum finibus varius. Duis porta orci sit amet leo
+vehicula semper. Suspendisse potenti. Nam erat ligula, facilisis eu fermentum ut, ornare nec
+felis.
+</p>
+<p>Aenean lobortis augue sit amet velit accumsan posuere. Nulla non aliquam nibh. Sed fringilla
+mauris quis bibendum varius. Proin a quam vestibulum, porttitor ante quis, volutpat augue.
+Phasellus ut condimentum orci. In nec vulputate purus. Integer pretium congue nisl, varius
+tristique orci lacinia ut. Sed bibendum, mauris id facilisis euismod, mauris magna mattis lacus,
+ac sagittis purus orci at arcu. Nulla eu quam feugiat, aliquam tortor eget, molestie enim. Sed
+molestie arcu non lorem porta dapibus. Mauris et consectetur justo. Cras ut laoreet enim. Sed
+ullamcorper mollis nisi, sed lacinia ex tincidunt a.
+</p>
+<p>Vivamus quam ante, volutpat eu ex vitae, molestie ullamcorper sem. Fusce non metus scelerisque,
+placerat massa sed, pretium lorem. Vivamus rhoncus posuere efficitur. Nulla faucibus felis vitae
+turpis rhoncus dignissim. Aliquam erat volutpat. Pellentesque sagittis massa at ipsum mattis
+gravida. Pellentesque lacinia sapien et augue suscipit ultrices. Proin cursus venenatis egestas.
+Nam pretium sapien id risus pretium convallis. Maecenas finibus pellentesque dignissim.
+Phasellus vel ex hendrerit, hendrerit urna eget, auctor mauris. Ut tempus id felis dictum
+volutpat. Morbi quis efficitur felis.</p>
+`
